d5e1e11e61e55101dac9f48ff77d894d87c27f4b
reteza <81977235+reteza@users.noreply.github.com>
2023-02-07 14:44:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg; 
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+  galleryImages.forEach(function(image, index) {
+    image.onclick = function() {
+        let getElementCss = window.getComputedStyle(image);
+        let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+        let getImgUrlPos = getFullImgUrl.split("/img/thumbs/");
+        let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+        getLatestOpenedImg = index + 1;
+
+        let container = document.body;
+        let newImgWindow = document.createElement("div");
+        container.appendChild(newImgWindow);
+        newImgWindow.setAttribute("class", "img-window");
+        newImgWindow.setAttribute("onclick", "closeImg()");
+
+        let newImg = document.createElement("img");
+        newImgWindow.appendChild(newImg);
+        newImg.setAttribute("src", "img/" + setNewImgUrl);
+         
+   }
+  });
+}
 const swiper = new Swiper('.swiper',{
     loop: true,
     direction: 'vertical',
@@ -14,3 +41,4 @@ const swiper = new Swiper('.swiper',{
     },
 
   })
+
