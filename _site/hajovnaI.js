c8fba1531c0e32970f306f5ab03cd83e3c284fd3
reteza <81977235+reteza@users.noreply.github.com>
2023-10-30 13:25:29 +0100

    Add files via upload

---
+++ _site/hajovnaI.js
@@ -0,0 +1,35 @@
+let galleryImages = document.querySelectorAll(".galleryHajovnaI-img");
+let getLatestOpenedImg; 
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+  galleryImages.forEach(function(image, index) {
+    image.onclick = function() {
+      let getElementCss = window.getComputedStyle(image);
+      let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+      let getImgUrlPos = getFullImgUrl.split("/galerieHajovnaI/thumb/");
+      let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+      getLatestOpenedImg = index + 1;
+
+      let container = document.body;
+      let newImgWindow = document.createElement("div");
+      container.appendChild(newImgWindow);
+      newImgWindow.setAttribute("class", "img-window");
+      newImgWindow.setAttribute("onclick", "closeImg()");
+
+      let newImg = document.createElement("img");
+      newImgWindow.appendChild(newImg);
+      newImg.setAttribute("src", "/images/galerieHajovnaI/" + setNewImgUrl);
+
+   }
+  });
+
+}
+
+function closeImg() {
+  document.querySelector(".img-window").remove();
+  document.querySelector(".img-btn-next").remove();
+  document.querySelector(".img-btn-prev").remove();
+}
+
